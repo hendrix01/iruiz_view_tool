766c9fd5a3e99691ac7cf3ac5b7564706c153b2b
Ivan Ernesto Ruiz <hendrix01@users.noreply.github.com>
2020-03-30 11:33:42 -0700

    Ruiz tool initial commit

---
+++ lib/iruiz_view_tool.rb
@@ -0,0 +1,7 @@
+require "iruiz_view_tool/version"
+require "iruiz_view_tool/renderer"
+
+module IruizViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/iruiz_view_tool/version.rb
@@ -0,0 +1,3 @@
+module IruizViewTool
+  VERSION = "0.1.0"
+end
